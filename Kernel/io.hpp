3fa00d0e280bca7e5e25182650db1c02ce45e955
MinusGix <MinusGix@gmail.com>
2019-10-28 01:16:31 -0500

    Remove old definitions

--- Kernel/io.hpp
+++ Kernel/io.hpp
@@ -87,10 +87,5 @@ namespace SerialPort {
     void writeChar (COMPort com, char c);
     void writeString (COMPort com, const char* str, size_t length);
 };
-#define SERIAL_COM1_BASE 0x3F8
-void serial_configure_baud_rate (uint16_t com, uint16_t divisor);
-void serial_configure_line (uint16_t com);
-int serial_transmit_fifo_empty (uint32_t com);
-int serial_write (uint32_t com, const char* str, size_t length);
 
 #endif
