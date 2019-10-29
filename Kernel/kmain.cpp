0010df3b01beaea37f56d972241feef3573eb8b9
MinusGix <MinusGix@gmail.com>
2019-10-29 17:20:24 -0500

    Various modifications

--- Kernel/kmain.cpp
+++ Kernel/kmain.cpp
@@ -1,18 +1,34 @@
 #include "io.hpp"
 #include "general_assembly.hpp"
 #include "descriptor_tables.hpp"
+#include "context.hpp"
+#include "../Include/kcstring.hpp"
 
 extern "C" void kmain () {
     init_descriptor_tables();
 
+    // Enable interrupts again
+    __asm__ volatile ("sti");
+
+    // Remap the PIC interrupts to the correct places.
+    PIC::remapPIC();
+    outbyte(PIC::PIC1::data, 0b11111101);
+    outbyte(PIC::PIC2::data, 0b11111111);
+
     FrameBuffer::setCursorPosition(0, 0);
     char* alpha = "Starting up";
-    FrameBuffer::writeString(alpha, 5);
+    FrameBuffer::writeString(alpha, KCString::getLength(alpha), 2, 8);
+
+    if (areInterruptsEnabled()) {
+        FrameBuffer::writeCell('1', 3, 8);
+    } else {
+        FrameBuffer::writeCell('0', 3, 8);
+    }
 
     SerialPort::configureBaudRate(SerialPort::COM1, 0x0006);
     SerialPort::configureLine(SerialPort::COM1);
     SerialPort::writeString(SerialPort::COM1, alpha, 6);
 
-    asm volatile ("int $0x3");
-    asm volatile ("int $0x4");
+    __asm__ volatile ("int $0x3");
+    __asm__ volatile ("int $0x4");
 }
