7d8fe683d266c379705b0eadb69a28d2e3bed80a
MinusGix <MinusGix@gmail.com>
2019-10-31 03:49:04 -0500

    Handle all interrupts, and improve handling

--- Kernel/isr.cpp
+++ Kernel/isr.cpp
@@ -2,7 +2,16 @@
 #include "io.hpp"
 #include "../Include/kcstring.hpp"
 
+Function<void(Registers, int32_t, StackState)> interrupt_handlers[interrupt_handler_count];
+
+void initialiseInterrupts () {}
+
 extern "C" void isr_handler (Registers regs, uint32_t int_number, StackState state) {
+    if (int_number < interrupt_handler_count && interrupt_handlers[int_number].hasFunction()) {
+        interrupt_handlers[int_number](regs, int_number, state);
+        return;
+    }
+
     const char* text = "Unhandled INT ";
 
     SerialPort::writeString(SerialPort::COM1, text, KCString::getLength(text));
